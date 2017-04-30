7ceeee06a1659a68428e73e281dd56c4efa6ecfd
foxgem <jianhgreat@hotmail.com>
2017-04-30 11:36:34 +0800

    optimize process services templates

--- camunda/grails-app/services/my/TestProcessService.groovy
+++ camunda/grails-app/services/my/TestProcessService.groovy
@@ -1,7 +1,6 @@
 package my
 
-import org.camunda.bpm.engine.RuntimeService
-import org.camunda.bpm.engine.runtime.Execution
+import org.camunda.bpm.engine.delegate.DelegateExecution
 
 import java.util.logging.Logger
 
@@ -10,11 +9,9 @@ class TestProcessService {
     // Use java.util.logging in order to see something in vanilla tomcat catalina.out
     static Logger logger = Logger.getLogger(TestProcessService.name)
     
-    RuntimeService runtimeService
-
-    def serviceMethod(Execution execution) {
+    def serviceMethod(DelegateExecution execution) {
         println("${TestProcessService.class.simpleName} called from Camunda BPM ProcessInstance (id='${execution.processInstanceId}').")
-        runtimeService.getVariables(execution.processInstanceId).each {
+        execution.getVariables(execution.processInstanceId).each {
             logger.info("- Process Variable '${it.key}' = ${it.value} (${it.value?.class})")
             println("- Process Variable '${it.key}' = ${it.value} (${it.value?.class})")
         }
