e0936375cc4de459052459328082a67bdeba88c0
Feng Yu <abcfy2@163.com>
2018-01-23 14:28:11 +0800

    gorm use new java.time api

--- gorm-for-vertx/src/test/groovy/domain/PersonSpec.groovy
+++ gorm-for-vertx/src/test/groovy/domain/PersonSpec.groovy
@@ -1,5 +1,6 @@
 package domain
 
+import java.time.LocalDateTime
 import grails.gorm.transactions.Rollback
 import org.grails.orm.hibernate.HibernateDatastore
 import spock.lang.AutoCleanup
@@ -14,7 +15,7 @@ class PersonSpec extends Specification {
     @Rollback
     def "person can be safed with firstname and lastname"() {
         when:
-        def p = new Person(firstName: 'Sergio', lastName: 'del Amo')
+        def p = new Person(firstName: 'Sergio', lastName: 'del Amo', createTime: LocalDateTime.now())
         p.save(flush: true)
 
         then:
